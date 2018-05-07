0fdcb01250df90670ae73a94825f2fc24f5a7280
Paul Betts <paul@paulbetts.org>
2018-05-06 21:03:50 -0700

    Let's find some notez

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
-function findNotesDirectory() {
+enum NotesDirectoryType {
+  None = 0,
+  Local = 1,
+  Global = 2,
+}
+
+function findNotesDirectory(context: vscode.ExtensionContext) {
+  const toTry = [
+    { type: NotesDirectoryType.Local, state: context.workspaceState, },
+    { type: NotesDirectoryType.Global, state: context.globalState, }
+  ];
+
+  for (let { type, state } of toTry) {
+    let path = state.get<string>('notesRootPath');
+
+    if (path && fs.existsSync(path)) {
+      return { type, path };
+    }
+  }
+
+  return  { type: NotesDirectoryType.None, path: null };
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "vscode-ursa" is now active!');
